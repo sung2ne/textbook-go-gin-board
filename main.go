18a68e3612b4a4be161dd25ed629f7a3276498ae
sung2ne <ibetter.kr@gmail.com>
2026-06-19 19:01:24 +0900

    part02/chapter-03: 01. 요청 바인딩

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+    "net/http"
+    "path/filepath"
+
     "github.com/gin-gonic/gin"
 )
 
 func main() {
     r := gin.Default()
 
-    // 정적 파일 서빙
-    r.Static("/static", "./public")
+    // 업로드 파일 크기 제한 (8MB)
+    r.MaxMultipartMemory = 8 << 20
 
-    // 메인 페이지
-    r.StaticFile("/", "./public/index.html")
+    r.POST("/upload", func(c *gin.Context) {
+        // 폼에서 파일 가져오기
+        file, err := c.FormFile("file")
+        if err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+
+        // 파일 저장
+        filename := filepath.Base(file.Filename)
+        dst := "./uploads/" + filename
+
+        if err := c.SaveUploadedFile(file, dst); err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+            return
+        }
+
+        c.JSON(http.StatusOK, gin.H{
+            "filename": filename,
+            "size":     file.Size,
+        })
+    })
 
     r.Run(":8080")
 }
