4536888aa64b4e6ae6b630c479e6301255935ace
sung2ne <ibetter.kr@gmail.com>
2026-06-19 19:01:24 +0900

    part01/chapter-03: 01. 함수 정의

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "log"
+    "net/http"
+)
 
 func main() {
-    var name string
-    name = "Go"
-    fmt.Println(name)
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello, Go!"))
+    })
+
+    log.Println("서버 시작: http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
